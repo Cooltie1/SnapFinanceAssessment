cb79693f04aca24e05969934ae5c125063bde794
Scott Olson <scottolsonofficial@gmail.com>
2026-01-20 15:34:29 -0800

    Question 3

--- src/metrics.py
+++ src/metrics.py
@@ -22,3 +22,26 @@ def app_trends_by_period(apps: pd.DataFrame, freq: str = "W") -> pd.DataFrame:
     )
 
     return summaryTable
+
+def app_metrics_by_store(apps: pd.DataFrame) -> pd.DataFrame:
+
+    df= apps.copy()
+    df["submitted"] = 1
+    df["approved"] = df["approved_date"].notna().astype(int) # 1 if approved, 0 if approved_date is blank
+    df["used"] = (df["dollars_used"] > 0).astype(int)
+    df["pct_used"] = df["dollars_used"] / df["approved_amount"]
+
+    summaryTable = (
+        df.groupby("store", dropna=False)
+            .agg(
+                applications=("submitted","sum"),
+                approved=("approved", "sum"),
+                used=("used", "sum"),
+                avg_approved_amount = ("approved_amount", "mean"),
+                avg_used_amount = ("dollars_used", "mean"),
+                avg_pct_used=("pct_used", "mean")
+            )
+            .reset_index()
+    )
+
+    return summaryTable
